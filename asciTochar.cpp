f32876592459685f35f9f10f20ea158c6102fb10
Montassar <montassar.themri1@gmail.com>
2023-01-15 08:01:03 +0100

    convert Asci code to charater

---
+++ asciTochar.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void charTOasciw
+
+int main()
+{
+    int exit;
+    do
+    {
+        char inputT;
+        do
+        {
+            cout << "enter [A] for Asci; \t [C] for character , " << endl;
+            cin >> inputT;
+            if (inputT == char(0))
+                return 1;
+        } while (inputT != 'A' && inputT != 'C');
+
+        if (inputT == 'A')
+        {
+            int item;
+            cout << "insert a Asci code" << endl;
+            cin >> item;
+            cout << "the character of the asci code :" << item << " :==> is "
+                 << "[" << char(item) << "]" << endl;
+        }
+        else
+        {
+            char character;
+            cout << "insert a character" << endl;
+            cin >> character;
+            cout << "the ASCI of the character [" << character << "]:==>  is [" << int(character) << "]" << endl;
+        }
+        cout << "CLICK '1' IF YOU Want to continue and any key to exit \n";
+        cin >> exit;
+
+    } while (exit == 1);
+}
