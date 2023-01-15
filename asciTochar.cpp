62ef17496db0230c1afa3de238cf9038b5fa5d45
Montassar <montassar.themri1@gmail.com>
2023-01-15 10:08:15 +0100

    update function

--- asciTochar.cpp
+++ asciTochar.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void charTOasciw
+void charTOasci();
+void asciToChar();
 
 int main()
 {
@@ -19,21 +20,31 @@ int main()
 
         if (inputT == 'A')
         {
-            int item;
-            cout << "insert a Asci code" << endl;
-            cin >> item;
-            cout << "the character of the asci code :" << item << " :==> is "
-                 << "[" << char(item) << "]" << endl;
+            asciToChar();
         }
         else
         {
-            char character;
-            cout << "insert a character" << endl;
-            cin >> character;
-            cout << "the ASCI of the character [" << character << "]:==>  is [" << int(character) << "]" << endl;
+            charTOasci();
         }
         cout << "CLICK '1' IF YOU Want to continue and any key to exit \n";
         cin >> exit;
 
     } while (exit == 1);
+}
+
+void charTOasci()
+{
+    char character;
+    cout << "insert a character" << endl;
+    cin >> character;
+    cout << "the ASCI of the character [" << character << "]:==>  is [" << int(character) << "]" << endl;
+}
+
+void asciToChar()
+{
+    int item;
+    cout << "insert a Asci code" << endl;
+    cin >> item;
+    cout << "the character of the asci :" << item << " :==> is "
+         << "[" << char(item) << "]" << endl;
 }
